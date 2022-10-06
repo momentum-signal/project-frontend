54123468a508c86193a66d33aecaf265beb6926c
Saiket Das <saiketdas02@gmail.com>
2022-10-06 23:33:02 +0600

    Header Component Created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Button } from "@mui/material";
 import "./App.css";
+import Header from "./components/shared/Header";
 
 function App() {
   return (
     <div className="App">
-      <h2 className="text-5xl">Test title</h2>
+      {/* <h2 className="text-5xl">Test title</h2>
       <Button variant="contained">Contained</Button>
-      <p>Saiket Das</p>
+      <p>Saiket Das</p> */}
+
+      <Header></Header>
     </div>
   );
 }
